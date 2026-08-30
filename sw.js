8e1c78f2fc78c1bd8ae74ec9a741cd5ca27e4ada
Ariel Rahmanov <arielchai9400@gmail.com>
2026-08-30 12:26:32 -0400

    A trip that produced nothing says nothing, in a neutral tone, in both renderers

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mercy-v75';
+const CACHE = 'mercy-v76';
 const ASSETS = ['./', './index.html', './manifest.json',
   './icon-192.png', './icon-512.png',
   './icon-192-maskable.png', './icon-512-maskable.png',
