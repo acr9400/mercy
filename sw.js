6ca1c555a81d9534fb87e9889cd11555f43d731a
Ariel Rahmanov <arielchai9400@gmail.com>
2026-08-30 17:06:30 -0400

    Outside tags go green to match the histogram legend; tag borders follow the theme

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mercy-v77';
+const CACHE = 'mercy-v78';
 const ASSETS = ['./', './index.html', './manifest.json',
   './icon-192.png', './icon-512.png',
   './icon-192-maskable.png', './icon-512-maskable.png',
