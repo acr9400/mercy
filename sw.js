556879fb943b72aee4868197555ae48dadbca2d1
Ariel Rahmanov <arielchai9400@gmail.com>
2026-08-31 07:01:31 -0400

    Bump the cache to v84 for the overnight button

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mercy-v83';
+const CACHE = 'mercy-v84';
 const ASSETS = ['./', './index.html', './manifest.json',
   './icon-192.png', './icon-512.png',
   './icon-192-maskable.png', './icon-512-maskable.png',
