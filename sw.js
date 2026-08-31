b355fb09cec22251f4540dcd2f6e599ba9193fa8
Ariel Rahmanov <arielchai9400@gmail.com>
2026-08-31 09:11:09 -0400

    Bump the cache to v87

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mercy-v86';
+const CACHE = 'mercy-v87';
 const ASSETS = ['./', './index.html', './manifest.json',
   './icon-192.png', './icon-512.png',
   './icon-192-maskable.png', './icon-512-maskable.png',
