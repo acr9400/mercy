d6a9911f5421e1a19f9949718fe6fad30260c259
Ariel Rahmanov <arielchai9400@gmail.com>
2026-08-31 23:04:58 -0400

    v98: count outings, not rows. A walk and the pee she did on it are one trip

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mercy-v97';
+const CACHE = 'mercy-v98';
 const ASSETS = ['./', './index.html', './manifest.json',
   './icon-192.png', './icon-512.png',
   './icon-192-maskable.png', './icon-512-maskable.png',
