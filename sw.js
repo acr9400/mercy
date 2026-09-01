9cb96438c628568264ad7c26b0809e85a05515a7
Ariel Rahmanov <arielchai9400@gmail.com>
2026-08-31 20:40:10 -0400

    v89: three forecast bars under the clock, one per pee, poop and walk (sw.js)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mercy-v88';
+const CACHE = 'mercy-v89';
 const ASSETS = ['./', './index.html', './manifest.json',
   './icon-192.png', './icon-512.png',
   './icon-192-maskable.png', './icon-512-maskable.png',
