75ad481a516dbea8d85801e2aec2263c275f605a
Ariel Rahmanov <arielchai9400@gmail.com>
2026-08-31 22:28:54 -0400

    v95: a colour wash on every panel, and a poop bar that always has something to count

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mercy-v94';
+const CACHE = 'mercy-v95';
 const ASSETS = ['./', './index.html', './manifest.json',
   './icon-192.png', './icon-512.png',
   './icon-192-maskable.png', './icon-512-maskable.png',
