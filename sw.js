67f7d69a97cd0260d28827e0542c13f127d1a047
Ariel Rahmanov <arielchai9400@gmail.com>
2026-08-31 21:02:12 -0400

    v92: a bar empties when she has just been, on every panel

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mercy-v91';
+const CACHE = 'mercy-v92';
 const ASSETS = ['./', './index.html', './manifest.json',
   './icon-192.png', './icon-512.png',
   './icon-192-maskable.png', './icon-512-maskable.png',
